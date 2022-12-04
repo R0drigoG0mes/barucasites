f53503ca153a81134933c7cb4634cd64e693fd2d
Rodrigo Gomes Cordeiro <98043205+R0drigoG0mes@users.noreply.github.com>
2022-12-04 18:00:13 -0300

    cv

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,15 @@
+const nav_diferente = document.querySelector('.diferencialy');
+var nav_projetos = document.querySelector("#projetos");
+const nav_contato = document.getElementById("contato");
+const titulo = document.querySelectorAll("h1");
 
+// nav_projetos.addEventListener("click",verDiferente);
 
+nav_projetos.addEventListener("click", function(){alert("jnjbnhb");});
+
+// window.addEventListener("click", verDiferente)
+
+function verDiferente(){
+    // window.scroll(0,363);
+    alert("Knn");
+}
