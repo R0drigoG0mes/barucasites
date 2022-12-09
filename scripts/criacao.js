688f33c5af7aa7becebc08ec792e1eca53a65a15
Rodrigo Gomes Cordeiro <98043205+R0drigoG0mes@users.noreply.github.com>
2022-12-09 17:32:55 -0300

    avanço no projeto

--- scripts/criacao.js
+++ scripts/criacao.js
@@ -9,13 +9,13 @@ document.addEventListener("touchmove", function(e){
         toquesX.unshift(touch.pageX);
         
         if(toquesX[0] > toquesX[1] && toquesX[1] > toquesX[2] && toquesX[2] > toquesX[3]){
-            caixaFerramentas.style.animation = "surgirEsquerda 1.5s linear forwards";
+            caixaFerramentas.style.animation = "surgirEsquerda 1s linear forwards";
             frechinha.classList.remove("icon-circle-right");
             frechinha.classList.add("icon-circle-left");
             console.log('boa surgiu');
         }
         else if(toquesX[0] < toquesX[1] && toquesX[1] < toquesX[2] && toquesX[2] < toquesX[3]){
-            caixaFerramentas.style.animation = "sumirEsquerda 1.5s linear forwards";
+            caixaFerramentas.style.animation = "sumirEsquerda 1s linear forwards";
             frechinha.classList.remove("icon-circle-left");
             frechinha.classList.add("icon-circle-right");
             console.log('boa sumiu');
@@ -25,12 +25,12 @@ document.addEventListener("touchmove", function(e){
 
 frechinha.addEventListener("click", function(e){
     if(frechinha.classList.contains("icon-circle-right")){
-        caixaFerramentas.style.animation = "surgirEsquerda 1.5s linear forwards";
+        caixaFerramentas.style.animation = "surgirEsquerda 1s linear forwards";
         frechinha.classList.remove("icon-circle-right");
         frechinha.classList.add("icon-circle-left");
     }
     else{
-        caixaFerramentas.style.animation = "sumirEsquerda 1.5s linear forwards";
+        caixaFerramentas.style.animation = "sumirEsquerda 1s linear forwards";
         frechinha.classList.remove("icon-circle-left");
         frechinha.classList.add("icon-circle-right");
     }
