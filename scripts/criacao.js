68b9ff858e73ea9ed8ddfc8afb1370c5ebd54394
Rodrigo Gomes Cordeiro <98043205+R0drigoG0mes@users.noreply.github.com>
2022-12-14 12:54:53 -0300

    avanço

--- scripts/criacao.js
+++ scripts/criacao.js
@@ -6,13 +6,27 @@ const detalhes_li = document.getElementById("detalhes_li");
 
 // ---------- FERRAMENTAS ------------
 
+// ---------- PROPRIEDADES ------------
+
+const caixaPropriedades = document.getElementById("propriedades");
+
+// ---------- PROPRIEDADES ------------
+
 // -------------- ARRAYS ------------------
 
 var caminhoDoClique = [];
 var toquesX = [];
 var filhosDoClicado = [];
+var filhosDoMain = [];
 
 // -------------- ARRAYS ------------------
+
+// -------------- VARIÁVEIS ------------------
+
+var CaixaFerramentasAberta = false;
+var CaixaPropriedadesAberta = false;
+
+// -------------- VARIÁVEIS ------------------
 
 // ------------ CONSTANTES -----------------
 
@@ -23,6 +37,8 @@ const arquivos_imagens_input = document.getElementById("arquivo_img");
 const btn_upload = document.getElementById("btn_upload");
 const label = document.querySelector('.upload-img');
 const imagensParaAdicionarNoSite = document.querySelector('.imagem');
+const AvisoSemImagens = document.getElementById("SemImagens");
+const SiteCriado = document.getElementById("SiteCriado");
 
 // ------------ CONSTANTES -----------------
 
@@ -33,24 +49,52 @@ document.addEventListener("touchmove", function(e){
 
         toquesX.unshift(touch.pageX);
 
-        if(toquesX[0] > toquesX[1] && toquesX[1] > toquesX[2] && toquesX[2] > toquesX[3]){
+        if(toquesX[0] > toquesX[1] && toquesX[1] > toquesX[2] && toquesX[2] > toquesX[3] && CaixaPropriedadesAberta == false){
             caixaFerramentas.style.animation = "surgirEsquerda 1s linear forwards";
+            CaixaFerramentasAberta = true;
         }
-        else if(toquesX[0] < toquesX[1] && toquesX[1] < toquesX[2] && toquesX[2] < toquesX[3]){
+        else if(toquesX[0] < toquesX[1] && toquesX[1] < toquesX[2] && toquesX[2] < toquesX[3] && CaixaFerramentasAberta == true){
             caixaFerramentas.style.animation = "sumirEsquerda 1s linear forwards";
+            setTimeout(() => {CaixaFerramentasAberta = false}, 1100);
+            
+        }
+
+        else if(toquesX[0] < toquesX[1] && toquesX[1] < toquesX[2] && toquesX[2] < toquesX[3] && CaixaFerramentasAberta == false){
+            caixaPropriedades.style.animation = "surgirDireita 1s linear forwards";
+            CaixaPropriedadesAberta = true;
+        }
+        else if(toquesX[0] > toquesX[1] && toquesX[1] > toquesX[2] && toquesX[2] > toquesX[3] && CaixaPropriedadesAberta == true){
+            caixaPropriedades.style.animation = "sumirDireita 1s linear forwards";
+            setTimeout(() => {CaixaPropriedadesAberta = false}, 1100);
         }
     })
 })
 
+// ------------ ABRIR E FECHAR PELO BOTÃO ---------------
+
+document.addEventListener("click", function(e){
+    var caminhoDoClique2 = e.composedPath();
+
+    if(caminhoDoClique2[0] == '[object HTMLSpanElement]' && caminhoDoClique2[0].id == 'fecharFerramentas'){
+        caixaFerramentas.style.animation = "sumirEsquerda 1s linear forwards";
+        setTimeout(() => {CaixaFerramentasAberta = false}, 1100);
+    }
+    else if(caminhoDoClique2[0] == '[object HTMLSpanElement]' && caminhoDoClique2[0].id == 'fecharPropriedades'){
+        caixaPropriedades.style.animation = "sumirDireita 1s linear forwards";
+        setTimeout(() => {CaixaPropriedadesAberta = false}, 1100);
+    }
+})
+
+// ------------ ABRIR E FECHAR PELO BOTÃO ---------------
+
 // ------------ TOUCH ---------------
+
 
 // ----------- CLIQUES ---------------
 
 caixaFerramentas.addEventListener("click", function(e){
     caminhoDoClique = e.composedPath();
     filhosDoClicado = caminhoDoClique[0].children;
-
-    // caminhoDoClique[0] != '[object HTMLInputElement]' && caminhoDoClique[0] != '[object HTMLLabelElement]' && caminhoDoClique[0] != '[object HTMLSpanElement]' 
 
     if(caminhoDoClique[0] == '[object HTMLLIElement]' && filhosDoClicado[2] && filhosDoClicado[2].style.display == 'none'){
         filhosDoClicado[2].style.display = 'block';
@@ -82,6 +126,7 @@ arquivos_imagens_input.addEventListener('change', function(e){
             imagem.src = alvoLeitor.result;
             imagem.classList.add('imagem');
 
+            AvisoSemImagens.style.display = 'none';
             upload_imagens.insertBefore(imagem, btn_upload);
         })
 
@@ -96,9 +141,25 @@ document.addEventListener('click', function(e){
         var duplicata = document.createElement('img');
         duplicata.src = AlvoInicial[0].src;
         duplicata.classList.add('clones');
-        document.body.append(duplicata);
-        console.log(duplicata.src);
+        var PerguntaAdicionar = confirm('Quer adicionar essa imagem no seu site?')
+        if(PerguntaAdicionar == true){
+            SiteCriado.appendChild(duplicata)
+        }
     }
 })
 
 // ----------- UPLOAD IMAGENS --------
+
+// ------------------ PROPRIEDADES ----------
+
+SiteCriado.addEventListener("click", function(e){
+    var caminhoDoClique3 = e.composedPath();
+    filhosDoMain = SiteCriado.getElementsByTagName("*");
+
+    [...filhosDoMain].forEach(elemento =>{
+        elemento.classList.remove('selecionado');
+    })
+    caminhoDoClique3[0].classList.add('selecionado');
+})
+
+// ------------------ PROPRIEDADES ----------
